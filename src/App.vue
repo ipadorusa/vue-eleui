c8b09f7f45363cf50c36637471f5b0b95b49531f
ipadorusa <ipadorusa@gmail.com>
2018-07-08 22:18:20 +0900

    change commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {margin:0;padding:0}
 </style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,10 @@
 <template>
-
+  <div>
+    <h1>기본페이지 입니다.</h1>
+  </div>
 </template>
 <script>
-
+export default {
+  name: 'Index'
+}
 </script>
