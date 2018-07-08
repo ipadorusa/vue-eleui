b4b849ac4758755aa2c34150b6953019fda4ac71
ipadorusa <ipadorusa@gmail.com>
2018-07-09 00:21:29 +0900

    routerstudy-01

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <ul>
+          <li><router-link to="/">home</router-link></li>
+          <li><router-link to="/listbook">listbook</router-link></li>
+        </ul>
+      </el-aside>
       <el-main>
         <router-view></router-view>
       </el-main>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import ListBook from '@/components/ListBook'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/listbook',
+      name: 'ListBook',
+      component: ListBook
     }
   ]
 })
