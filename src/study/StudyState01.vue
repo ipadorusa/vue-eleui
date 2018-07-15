b88ff53af100e2f318c7f904c1b17b7d64bf5abe
ipadorusa <ipadorusa@gmail.com>
2018-07-15 23:32:14 +0900

    package revert and studylist update(v-bind)

--- src/study/StudyState01.vue
+++ src/study/StudyState01.vue
@@ -1,11 +1,14 @@
 <template>
     <section>
         <h2>v-bind</h2>
-        <div>
-            <span v-bind:title="message">
-            내 위에 잠시 마우스를 올리면 동적으로 바인딩 된 title을 볼 수 있습니다!
-            </span>
-        </div>
+        <br>
+        <p>
+            v-bind:src = 'expression'
+        </p>
+        <br>
+        <ul>
+            <li v-for="(list,index) in dataExam" :key="list.id">{{ index }}. {{ list.example }}</li>
+        </ul>
     </section>
 </template>
 <script>
@@ -13,7 +16,15 @@ export default {
   name: "StudyState01",
   data() {
     return {
-      message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+        dataExam: [
+            {'example' : ':src="image"'},
+            {'example' : ':alt="description"'},
+            {'example' : ':href="url"'},
+            {'example' : ':title="toolTip"'},
+            {'example' : ':class="isAcitve"'},
+            {'example' : ':style="isStyled"'},
+            {'example' : ':disabled="isDisabled"'}
+        ]
     };
   }
 };
--- src/study/StudyState06.vue
+++ src/study/StudyState06.vue
@@ -5,7 +5,7 @@
         <p>뒤집히도록 계산된 메시지: "{{ reversedMessage }}" </p><br><br>
         <h2>계산된 속성 vs 감시된 속성</h2>
         <div>{{ fullName }}</div>
-        <button type="button"@click="fullName='박 명수'">바꾸기</button>
+        <button type="button" @click="fullName='박 명수'">바꾸기</button>
     </section>
 </template>
 <script>
